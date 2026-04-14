23930f28a13366c3ffe93d636d6c4058f575900b
Evgeniy Titov <titevg@gmail.com>
2026-04-15 00:06:42 +0300

    add logging with print() to console

--- src/fetcher.py
+++ src/fetcher.py
@@ -1,8 +1,8 @@
 """ Модуль описывающий логику загрузки страниц """
-
 from src.request_handler import SafeRequest
 
 def fetch_page(url) -> str | None:
+    """ Функция получения текста кода страницы """
     headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
                       ' (KHTML, like Gecko) Chrome/144.0.0.0 YaBrowser/26.3.0.0 Safari/537.36'
--- src/parser.py
+++ src/parser.py
@@ -3,10 +3,14 @@ from bs4 import BeautifulSoup
 from src.fetcher import fetch_page
 
 def sections_parse() -> dict:
+    """ Получение словаря с категориями слов и индекса html-страницы для нее"""
+
     url = 'https://langwitch.ru/wordsets/'
     data_text = fetch_page(url)
     soup = BeautifulSoup(data_text, 'html.parser')
-    sections = soup.find('div', class_='wordsets_card').find_all('a', recursive=False)
+    sections = soup.find('div', class_='wordsets_card').find_all('a',
+                                                            recursive=False)
+    print(f'Найдено всего {len(sections)} категорий')
     sections_dict = {}
     for section in sections:
         all_text = section.get_text(strip=True)
@@ -15,25 +19,35 @@ def sections_parse() -> dict:
         url_attr = section.get('href')
         clear_url_attr = url_attr.replace('/wordsets/', '')
         sections_dict[clear_text] = clear_url_attr
+        print(f'Добавлена категория {clear_text}.\n '
+              f'Cсылка на категорию https://langwitch.ru/wordsets/{clear_url_attr}')
     return sections_dict
 
 def parse_section(section) -> dict:
+    """ Получение словаря {Ru:En} для каждой категории """
     new_url = f'https://langwitch.ru/wordsets/{section}'
     data_text = fetch_page(new_url)
     soup = BeautifulSoup(data_text, 'html.parser')
     sections = soup.find_all('div', class_='word_row')
     ru_en_words = {}
+    count = 0
     for section in sections:
         text_en = section.find('div', class_='word_row_en').get_text(strip=True)
         erase_text_en = section.find('div', class_='word_row_transcription').get_text()
         clear_text_en = text_en.replace(erase_text_en, '')
         text_ru = section.find('div', class_='word_row_ru').get_text(strip=True).capitalize()
         ru_en_words[text_ru] = clear_text_en
+        count += 1
+    print(f'Найдено {count} слов для категории {section}(техническое название)')
     return ru_en_words
 
 def full_parser() -> dict:
+    """ Получение полного словаря слов, разбитого по категориям"""
+
     all_dict = {}
     sections = sections_parse()
     for category, section in sections.items():
         all_dict[category] = parse_section(section)
+        print(f'Добавлен словарь для категории {category}')
+    print('Парсинг выполнен!')
     return all_dict
